a417a665b6534b39b9de837bf6ad9fe9da595c63
Leo Pinard <yaconsult@gmail.com>
2026-04-14 13:53:25 -0700

    Restore main.py to simple splash (font boot issue TBD)

--- main.py
+++ main.py
@@ -9,21 +9,15 @@ After flashing, every power-cycle or replug will:
 The `d` object is then available in the REPL.
 """
 
-from writer import Writer
-import fonts.sans16 as sans16
-import fonts.sans24 as sans24
 from display import Display
 
-import time
 d = Display(brightness=80)
-d.fb.fill(d.BLACK)
-d.show()
-time.sleep_ms(500)  # let DMA settle after first draw
 
-wri24 = Writer(d.fb, sans24, verbose=False)
-Writer.set_textpos(d.fb, 10, 10)
-wri24.setcolor(d.YELLOW, d.BLACK)
-wri24.printstring('Hello!')
+d.fb.fill(d.BLACK)
+cx = (d.width - 13*8) // 2
+d.text('JC3248W535EN', cx, d.height//2 - 20, d.WHITE)
+d.text('MicroPython', (d.width - 11*8)//2, d.height//2, d.color(180, 220, 255))
+d.text('Ready.', (d.width - 6*8)//2, d.height//2 + 20, d.YELLOW)
 d.show()
 
 print('Display ready.')
